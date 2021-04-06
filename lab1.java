edf9c3d5c2055370db18b6c95326999e6cec7b80
wiwatyooraks <78940910+wiwatyooraks@users.noreply.github.com>
2021-04-06 14:01:31 +0700

    JAVA1-2.1

---
+++ lab1.java
@@ -0,0 +1,23 @@
+package com.wiwatyooraks.helloworld;
+
+public class lab1 {
+	
+//comment 1
+	
+/*
+  comment 2
+*/
+
+	public static void main(String[] args) {
+		int number = 15;
+		boolean myBoolean = false;
+		String greeting = "hello";
+		byte myByte = 0;
+		short myShort = 0;
+	    System.out.println("This is a int : " + number);
+	    System.out.println("This is a boolean : " + myBoolean);
+	    System.out.println("This is a String : " + greeting);
+	    System.out.println("This is a byte : " + myByte);
+	    System.out.println("This is a short : " + myShort);
+	}
+}
