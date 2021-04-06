720976c4f7a66bf5cd333e1ce0bec181ea96cf67
wiwatyooraks <78940910+wiwatyooraks@users.noreply.github.com>
2021-04-06 14:12:07 +0700

    JAVA1-5.0

---
+++ java1lab11/lab11.java
@@ -0,0 +1,15 @@
+package com.wiwatyooraks.java1lab11;
+
+public class lab11 {
+	public static void main(String[] args) {
+		String myWord = "Hello World";
+		System.out.println(greeting(myWord));
+		sayHello(myWord);
+	}
+		public static String greeting(String word) {
+		return word;
+	    }
+		public static void sayHello(String word) {
+			System.out.println(word);
+		}
+}
